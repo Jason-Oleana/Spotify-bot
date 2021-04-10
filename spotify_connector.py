fdeab7781d35c903c58750fa65c1f7cbc285650b
jason <jason@geniusvoice.nl>
2021-04-10 17:05:15 +0200

    Update: .env and spotify connector

---
+++ spotify_connector.py
@@ -0,0 +1,31 @@
+import requests
+import os
+
+token = os.getenv("SPOTIFY_TOKEN")
+api_url = os.getenv("SPOTIFY_URL")
+
+
+def get_playlists(token, api_url):
+
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+    }
+
+    response = requests.get(api_url + 'users/11143403508/playlists', headers=headers)
+    print("playlists :{}\n".format(response.text))
+
+def get_tracks(token, api_url):
+
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+    }
+
+    response = requests.get(api_url + 'https://api.spotify.com/v1/artists/11143403508/tracks', headers=headers)
+    print("tracks: {}".format(response.text))
+
+get_playlists(token, api_url)
+get_tracks(token, api_url)
