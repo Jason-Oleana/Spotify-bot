93aa0ed9ea6e07c4851aebf004ab690347cbccfa
jason <jason@geniusvoice.nl>
2021-04-11 14:50:10 +0200

    Update: headers

--- spotify_connector.py
+++ spotify_connector.py
@@ -4,25 +4,17 @@ import os
 token = os.getenv("SPOTIFY_TOKEN")
 api_url = os.getenv("SPOTIFY_URL")
 
+headers = {'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token}
 
-def get_playlists(token, api_url):
 
-    headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + token,
-    }
+def get_playlists(token, api_url, headers):
 
     response = requests.get(api_url + 'users/11143403508/playlists', headers=headers)
     print("playlists :{}\n".format(response.text))
 
-def get_tracks(token, api_url):
-
-    headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + token,
-    }
+def get_tracks(token, api_url, headers):
 
     response = requests.get(api_url + 'artists/11143403508/tracks', headers=headers)
     print("tracks: {}".format(response.text))
